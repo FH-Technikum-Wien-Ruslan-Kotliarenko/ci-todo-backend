27ff7672c08831c66746090ac2b6e17af89d448b
Ruslan Kotliarenko <ruslankot101@gmail.com>
2025-09-12 11:27:01 +0200

    test

--- src/db.js
+++ src/db.js
@@ -4,8 +4,10 @@ const { DB_DIALECT = 'mariadb', DB_USER, DB_PW, DB_HOST, DB_NAME } = process.env
 
 const connUrl = `${DB_DIALECT}://${DB_USER}:${DB_PW}@${DB_HOST}/${DB_NAME}`;
 
+console.log('DB URL:', connUrl);
+
 const sequelize = new Sequelize(connUrl, {
-  logging: false, // flip to console.log if you need SQL logs
+  logging: console.log,
   dialectOptions: {
     // add RDS SSL options here if required by your instance
   }
